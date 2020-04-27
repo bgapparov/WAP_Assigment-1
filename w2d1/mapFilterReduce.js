5900fba54b8bdff9476e6063cf999846b4ed5ba1
Bayaman Gapparov <bayamangapparov@gmail.com>
2020-04-27 15:51:35 -0500

    lab 6 part 1

---
+++ w2d1/mapFilterReduce.js
@@ -0,0 +1,45 @@
+function functionTest(expected, reality) {
+    if(expected === reality){
+        return "TEST SUCCEEDED"
+    }else {
+        return "TEST FAILED. Expected " + expected + " reality " + reality;
+    }
+}
+
+//sum function with reduce
+
+function sum(array) {
+    var result = array.reduce((a, b) => a+ b, 0);
+    return result;
+}
+
+//multiply function with reduce
+function multiply(array) {
+    var result = array.reduce((a , b) => a * b, 1);
+    return result;
+}
+
+function reverseReduce(str) {
+    var revString = str.split("").reduce((rev, char)=> char + rev, '');
+    return revString;
+}
+
+//filterLong Words function with filter
+function filterLongWords(array, num){
+
+    var res = array.filter(function (a) {return a.length > num  });
+    return res.toString();
+}
+
+
+
+
+console.log("Expected output of the [3,4,5,6,7] is 25 " + functionTest(25, sum([1,3,5,7,4,5])));
+console.log("Expected output of the [1, 3, 4, 2] is 24 " + functionTest(24, multiply([3, 4, 2])));
+console.log("Expected output of  of the today  is yadot " + functionTest('yadot', reverseReduce('today')));
+console.log("Expected output of the ['Ale', 'Hello', 'Oha', 'u'] " + functionTest("Hello", filterLongWords(['Ale', 'Hello', 'Oha', 'u'], 3)))
+
+console.log(sum([1,3,5,7,4,5]));
+console.log(multiply([3, 4, 2]));
+console.log(reverseReduce('today'));
+console.log(filterLongWords(['Ale', 'Hello', 'Oha', 'u'], 3));
