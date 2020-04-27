a9e2b9d0be92ad6dfad86bc06cf71632f2a9fd81
Bayaman Gapparov <bayamangapparov@gmail.com>
2020-04-27 18:25:42 -0500

    Lab 6 part 2 done

---
+++ w2d1/decoraterJs.js
@@ -0,0 +1,27 @@
+window.onload = function () {
+    var bigDecButton = document.getElementById("bigDecId");
+    bigDecButton.onclick = helloClick;
+    document.getElementById("bling").onchange = onchange;
+};
+
+function helloClick() {
+   //document.getElementById("sampleId").style.fontSize = '24px';
+    let fontSize = parseInt(window.getComputedStyle(document.getElementById("sampleId")).fontSize);
+    fontSize += 5;
+
+    document.getElementById("sampleId").style.fontSize = fontSize + 'pt';
+}
+
+function onchange(id) {
+    if(this.checked) {
+        alert("Do you want to change some style?")
+        document.body.style.backgroundImage = "url('http://www.cs.washington.edu/education/courses/190m/CurrentQtr/labs/6/hundred-dollar-bill.jpg')";
+        document.getElementById("sampleId").style.fontWeight = 'bold';
+        document.getElementById("sampleId").style.color = 'green';
+        document.getElementById("sampleId").style.textDecoration= 'underline overline';
+    }else{
+        document.getElementById("sampleId").style.fontWeight = 'normal';
+        document.getElementById("sampleId").style.color = 'black';
+        document.body.style.backgroundImage = "url('none')";
+    }
+}
