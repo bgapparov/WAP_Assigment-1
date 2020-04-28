a8d8184637a2844f54b9fa83c33b393cdc184255
Bayaman Gapparov <bayamangapparov@gmail.com>
2020-04-27 19:09:53 -0500

    Time for increasing font size

--- w2d1/decoraterJs.js
+++ w2d1/decoraterJs.js
@@ -2,6 +2,7 @@ window.onload = function () {
     var bigDecButton = document.getElementById("bigDecId");
     bigDecButton.onclick = helloClick;
     document.getElementById("bling").onchange = onchange;
+    document.getElementById('bigDecId').onclick = timer;
 };
 
 function helloClick() {
@@ -14,7 +15,7 @@ function helloClick() {
 
 function onchange(id) {
     if(this.checked) {
-        alert("Do you want to change some style?")
+        alert("Do you want to change some style?");
         document.body.style.backgroundImage = "url('http://www.cs.washington.edu/education/courses/190m/CurrentQtr/labs/6/hundred-dollar-bill.jpg')";
         document.getElementById("sampleId").style.fontWeight = 'bold';
         document.getElementById("sampleId").style.color = 'green';
@@ -24,4 +25,8 @@ function onchange(id) {
         document.getElementById("sampleId").style.color = 'black';
         document.body.style.backgroundImage = "url('none')";
     }
+}
+
+function timer() {
+    setInterval(helloClick, 5000);
 }
