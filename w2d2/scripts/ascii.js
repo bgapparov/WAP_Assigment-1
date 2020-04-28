b2ef4aa86e0dfec813ee15f79ec7f7dfe606210d
Bayaman Gapparov <bayamangapparov@gmail.com>
2020-04-28 18:35:54 -0500

    Animation worked

---
+++ w2d2/scripts/ascii.js
@@ -0,0 +1,81 @@
+/*jshint esversion: 6 */
+(function() {
+    "use strict";
+    let isStarted = false;
+    let speed = 250;
+    let pos = 0;
+    let animationArr = [];
+    let animation = "Blank";
+
+    window.onload = ()=>{
+        isStarted = false;
+        document.getElementById("start").onclick = startOrStop;
+        document.getElementById("stop").onclick = startOrStop;
+        document.getElementById("animation").onchange = animationChanged;
+        document.getElementById("turbo").onchange = turboChanged;
+        document.getElementById("fontsize").onchange = fontSizeChanged;
+
+    };
+
+    function fontSizeChanged() {
+        const textArea = document.getElementById("text-area");
+        let fontSize = "12pt";
+        switch (document.getElementById("fontsize").value) {
+            case "Tiny":
+                fontSize = "7pt";
+                break;
+            case "Small":
+                fontSize = "10pt";
+                break;
+            case "Medium":
+                fontSize = "12pt";
+                break;
+            case "Large":
+                fontSize = "16pt";
+                break;
+            case "Extra Large":
+                fontSize = "24pt";
+                break;
+            case "XXL":
+                fontSize = "32pt";
+                break;
+        }
+        textArea.style.fontSize = fontSize;
+    }
+
+    function turboChanged() {
+        if(document.getElementById("turbo").checked){
+            speed = 50;
+        }else{
+            speed = 250;
+        }
+    }
+
+    function animationChanged(e) {
+        animation = document.getElementById("animation").value;
+        pos = 0;
+        animationArr = ANIMATIONS[animation].split("=====\n");
+
+    }
+
+    function startOrStop() {
+        isStarted = !isStarted;
+        pos = 0;
+        animationArr = ANIMATIONS[animation].split("=====\n");
+        document.getElementById("start").disabled = isStarted;
+        document.getElementById("stop").disabled = !isStarted;
+
+        if(isStarted){
+            run();
+        }
+    }
+
+    function run() {
+        document.getElementById("text-area").value = animationArr[pos%animationArr.length];
+        pos++;
+        if(isStarted){
+            setTimeout(run,  speed);
+        }
+    }
+
+}());
