a3b27b22ca0fe13beecaa6cba7972c71af3e2998
Bayaman Gapparov <bayamangapparov@gmail.com>
2020-04-28 21:21:53 -0500

    Unit test with macho

--- w2d2/scripts/basiciJS.js
+++ w2d2/scripts/basiciJS.js
@@ -3,45 +3,92 @@
 (function() {
     "use strict";
 
-    function functionTest(expected, reality) {
-        if(expected === reality){
-            return "TEST SUCCEEDED";
-        }else {
-            return "TEST FAILED. Expected " + expected + " reality " + reality;
-        }
-    }
+    let assign = {
+        array: [],
 
     //max2 return of the maximum of 2 numbers
-    function max(number1, number2){
-        if(number1 > number2){
+     max(number1, number2) {
+        if (number1 > number2) {
             return number1;
-        }
-        else {
+        } else {
             return number2;
         }
-    }
+    },
 
     //max3 takes three as arguments and returns the larges
-    function maxOfThree(number1, number2, number3){
-        if(number1 >= number2 && number2 >= number3){
+    maxOfThree(number1, number2, number3) {
+        if (number1 >= number2 && number2 >= number3) {
             return number1;
-        }else if(number2 >= number1 && number1 >= number3){
-            return  number2;
+        } else if (number2 >= number1 && number1 >= number3) {
+            return number2;
+        } else {
+            return number3;
         }
-        else {
-            return  number3;
-        }
-    }
+    },
 
-    function sum(array) {
-        var result = array.reduce((a, b) => a+ b, 0);
-        return result;
-    }
+        isVowel(letter){
+            let vowels = /^[aeiou]$/i;
+            return vowels.test(letter);
+        },
+
+    sum() {
+        return this.array.reduce((a, b) => a + b, 0);
+    },
 
 //multiply function with reduce
-    function multiply(array) {
-        var result = array.reduce((a , b) => a * b, 1);
-        return result;
-    }
+    multiply() {
+        return  this.array.reduce((a, b) => a * b, 1);
+    },
+};
+
+    describe("assign", function () {
+        function checkArray(result, resultMust){
+            assert.isArray(result, "result must be array");
+            assert.equal(result.length, resultMust.length, "Result array must have length = "+resultMust.length);
+            for(let indx in resultMust){
+                assert.equal(result[indx], resultMust[indx], "result["+indx+"] is "+result[indx]+". It must be "+resultMust[indx]);
+            }
+        }
+
+        context("When we find max number", function () {
+            beforeEach(function () {
+
+            });
+            it('the max of (25, 35) is 35', function () {
+                assert.equal(assign.max(25, 35), 35);
+            });
+            it('should the max of three number(10, 12, 15) is 15', function () {
+                assert.equal(assign.maxOfThree(10,12,15), 15);
+            });
+            it("the maxOfTree(41, 20, 30) is 41 (specially made mistake)", function () {
+                assert.equal(assign.maxOfThree(41, 20,30), 20);
+            });
+        });
+
+        context("When given an array [3, 5, 7, 8]", function () {
+            beforeEach(function () {
+                assign.array = [3, 5, 7, 10];
+            });
+            it('the sum() is 25', function () {
+                assert.equal(assign.sum(), 25);
+            });
+            it("ths multiply() is 120", function () {
+                assert.equal(assign.multiply(), 1050);
+            });
+        });
+
+        context("letter isVowel()", function () {
+            beforeEach(function () {
+
+            });
+            it('the isVowel("g") is False', function () {
+                assert.isFalse(assign.isVowel("g"), "g is not vowel");
+            });
+            it('the isVowel("e") is True', function () {
+                assert.isTrue(assign.isVowel("e"), "e is vowel");
+            });
+        });
+
+    });
 
 }());
